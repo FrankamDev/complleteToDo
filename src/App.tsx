c4557142c5abf84a709df549eb2517873de93085
Frankam487 <fkamgang06@gmail.com>
2025-07-10 10:40:32 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
+import { useState } from "react";
 
-
-type Priority = "Urgente" | "Moyenne" | "Peu Importante";
+type Priority = "Urgente" | "Moyenne" | "Basse";
 
 type Todo = {
   id: number;
   text: string;
   priority: Priority;
-}
+};
+
 const App = () => {
+  const [input, setInput] = useState<string>("test");
+  const [priority, setPriority] = useState<Priority>("Moyenne");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = () => {
+    if (input.trim() === "") {
+      return false;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: input.trim(),
+      priority: priority,
+    };
+
+    const newTodos = [newTodo, ...todos];
+    setTodos(newTodos);
+    setInput("");
+    setPriority("Moyenne");
+    console.log(todos);
+  };
+
   return (
-    <div className=" flex justify-center">
-      <div className="w-2/3 flex flex-col gap-4 bg-base-300 p-5 rounded-2xl">
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col gap-4 my-16 bg-base-300 p-5 rounded-2xl">
         <div className="flex gap-4">
           <input
             type="text"
-          className="input input-bordered w-full"
+            className="input w-full"
+            placeholder="Ajouter une tâche"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
-      </div>
+          <select
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as Priority)}
+            className="select w-full"
+          >
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Peu urgente</option>
+          </select>
+          <button onClick={addTodo} className="btn btn-primary">
+            Ajouter
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
