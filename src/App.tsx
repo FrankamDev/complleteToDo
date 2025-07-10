01ed6eba80e2663e1231bcbc1b5daf425e7c9fbf
Frankam487 <fkamgang06@gmail.com>
2025-07-10 09:31:08 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Home } from 'lucide-react';
+
+function MonComposant() {
+  return (
+    <div>
+      <h1>Accueil</h1>
+      <Home size={32} className='text-red-200' />
+    </div>
+  );
+}
+
+export default MonComposant;
